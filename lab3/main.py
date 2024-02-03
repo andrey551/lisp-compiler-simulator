56fbbd4a6c6e1df1ddbd79e295dc40d043df0955
tad <tuananhdaucong@gmail.com>
2024-02-03 20:59:19 +0300

    update algorithm build node tree

--- lab3/main.py
+++ lab3/main.py
@@ -20,25 +20,42 @@ class node():
         self.children = children
         self.level = level
     def print(self):
-        print("level: ", self.level, ", value: ", self.value)
+        print(self.level * " ", "value: ", self.value)
+        for i in self.children:
+            i.print()
+    def add_child(self, child):
+        self.children.append(child)
+    def get_value(self):
+        return self.value
 
 arr = []
 for i in range(len(arr1)):
     arr.append(node(arr1[i]))
 
+def get_values(arr):
+    ret = []
+    for i in arr:
+        ret.append({i.get_value(): len(i.children)})
+    return ret
+
 def execute(arr, index : int = 0):
     if(arr[index].value == '('):
-        arr[index] = node(-1, arr[index].level + 1)
+        arr[index] = node(-1, arr[index].level)
         while(arr[index + 1].value != ')'):
+            arr[index + 1].level = arr[index].level + 1
             execute(arr, index + 1)
-            arr[index].children.append(arr[index + 1])
+            arr[index].add_child(arr[index + 1])
             arr.pop(index + 1)
+        arr.pop(index + 1)
     elif(arr[index].value == ')'):
-        arr.pop(index)
+        pass
     else:
+        arr[index].children = []
         for i in range(arr[index].value):
+            arr[index + 1].level = arr[index].level + 1
             execute(arr, index + 1)
-            arr[index].children.append(arr[index + 1])
+            arr[index].add_child(arr[index + 1])
             arr.pop(index + 1)
+    print(index, get_values(arr))
 execute(arr, 0)
 arr[0].print()
