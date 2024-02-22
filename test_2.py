8bf5d24d3893c6de79f75d3f56590cba62e27f50
tad <tuananhdaucong@gmail.com>
2024-02-22 20:23:52 +0300

    d

---
+++ test_2.py
@@ -0,0 +1,38 @@
+from lab3.main import run
+import pytest
+import os
+import contextlib
+import io
+import logging
+
+@pytest.fixture
+@pytest.mark.golden_test("golden/testcase-2.yml")
+def test_hw(golden, caplog):
+    caplog.set_level(logging.DEBUG)
+    source =  "source.lisp"
+    input = "in.txt"
+    output = "out.txt"
+    debug_txt = "debug.txt"
+    mcode = "debug.bin"
+    
+    
+    with open(source, "w", encoding="utf-8") as file:
+        file.write(golden["source"])
+    
+    with open(input, "w", encoding="utf-8") as file:
+        file.write(golden["input"])
+
+    with contextlib.redirect_stdout(io.StringIO()) as stdout:
+        run(source, mcode, input, output, debug_txt)
+    
+
+    with open(output, encoding="utf-8") as file:
+        out = file.read()
+
+
+    with open(debug_txt, encoding="utf-8") as file:
+        stacktrace = file.read()
+
+    assert out == golden.out["output"]
+    assert stacktrace == golden.out["machine_code"]
+    # assert caplog.text == golden.out["log"]
